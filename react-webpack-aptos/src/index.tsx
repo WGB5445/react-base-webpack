1824f4c4267bf778a1df91a0a714286ad63088ce
WGB5445 <919603023@qq.com>
2022-08-27 18:16:28 +0800

    Add aptos Petra wallet

--- react-webpack-aptos/src/index.tsx
+++ react-webpack-aptos/src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 
 declare global {
   interface Window {
-    martian: any
+    martian: any,
+    aptos: any
   }
 }
 
-function getState(account:string , martian:boolean){
-  if(martian){
+function getState(account:string , state:boolean){
+  if(state){
     return  account?account:'Connect Wallet'
   }else{
     return 'install Wallet'
@@ -17,8 +18,11 @@ function getState(account:string , martian:boolean){
 
 function App(){
   const [martian,setMartian] = useState(false)
-  const [account,setAccount] = useState('')
-  const [networkId,setNetworkId] = useState(0)
+  const [aptos, setAptos] = useState(false)
+  const [martian_account,setMartian_Account] = useState('')
+  const [aptos_account,setAptos_Account] = useState('')
+  const [aptos_network,setAptos_Network] = useState('')
+  const [martian_networkId,setMartian_NetworkId] = useState(0)
   
 
   useEffect(()=>{
@@ -27,7 +31,15 @@ function App(){
 
       (async ()=> {
         const chainid = await window.martian.getChainId()
-        setNetworkId(chainid.chainId)
+        setMartian_NetworkId(chainid.chainId)
+      })()
+    }
+    if(window.aptos){
+      setAptos(true);
+
+      (async ()=> {
+        const network = await window.aptos.network()
+        setAptos_Network(network)
       })()
     }
   },[])
@@ -37,15 +49,31 @@ function App(){
       <button onClick={async ()=>{
         if(martian){
           const connect = await window.martian.connect();
-          setAccount(connect.address)
+          setMartian_Account(connect.address)
         }else{
           window.open("https://www.martianwallet.xyz/", "_blank");
         }
       }}>{
-        getState(account,martian)
+        getState(martian_account,martian)
       }</button>
       <p/>
-      <a>{"Network id : " + (networkId != 0 ? networkId.toString() : '')}</a>
+      <a>{"Network id : " + (martian_networkId != 0 ? martian_networkId.toString() : '')}</a>
+      <p/>
+      <a></a>
+
+      <h1> My React and TypeScript aptos Petra App!</h1>
+      <button onClick={async ()=>{
+        if(martian){
+          const connect = await window.aptos.connect();
+          setAptos_Account(connect.address)
+        }else{
+          window.open("https://github.com/aptos-labs/aptos-core/releases/tag/wallet-v0.1.7", "_blank");
+        }
+      }}>{
+        getState(aptos_account,aptos)
+      }</button>
+      <p/>
+      <a>{"Network id : " + (aptos_network ? aptos_network : '')}</a>
       <p/>
       <a></a>
     </div>
